46c99c56d4565f327ef772b850348d0f040285bb
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-28 18:04:28 +0530

    Add files via upload

---
+++ parallelinvoke/Class6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Iican
+{
+    class Class6
+    {
+        static void Main()
+        {
+            Parallel.Invoke(
+                    () => DoSomeTask(1),
+                    () => DoSomeTask(2),
+                    () => DoSomeTask(3),
+                    () => DoSomeTask(4),
+                    () => DoSomeTask(5),
+                    () => DoSomeTask(6),
+                    () => DoSomeTask(7)
+                );
+            Console.ReadKey();
+        }
+        static void DoSomeTask(int number)
+        {
+            //Console.WriteLine($"DoSomeTask {number} started by Thread {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"DoSomeTask  started by Thread {Thread.CurrentThread.ManagedThreadId}");
+            //Sleep for 5000 milliseconds
+            Thread.Sleep(5000);
+            //Console.WriteLine($"DoSomeTask {number} completed by Thread {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"DoSomeTask  completed by Thread {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
+
